35ccd77f92396df451fe19f35482fa32048d30d4
ghgg10345-sys <ghgg10345@gmail.com>
2026-07-01 07:26:38 +0530

    Swap values of a and b
    Replaced comparison logic with variable swapping and output.

--- Day1/Problem08.java
+++ Day1/Problem08.java
@@ -2,18 +2,16 @@ package Day1;
 
 public class Problem8 {
     public static void main(String[] args) {
-        int a=10;
-        int b=20;
-        int c=5;
-        if(a>b){
-            System.out.println("a is larger");
-        }
-        else if(b>c){
-            System.out.println("b is larger");
-        }
-        else{
-            System.out.println("c is larger");
-        }
+        
+        int a = 10;
+        int b = 20;
+        int temp;
+
+        temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
     }
-    
 }
