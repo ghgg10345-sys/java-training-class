765cf7fa071cac66fa4ea9b1e773ec007ed7284b
ghgg10345-sys <ghgg10345@gmail.com>
2026-07-01 07:33:15 +0530

    average of three numbers

--- Day1/problem13.java
+++ Day1/problem13.java
@@ -2,13 +2,13 @@ package Day1;
 
 public class problem13 {
     public static void main(String[] args) {
-        int n=20;
-        if(n%2==0){
-            System.out.println("even");
-        }
-        else{
-            System.out.println("odd");
-        }
+
+        int a = 10;
+        int b = 20;
+        int c = 30;
+
+        int average = (a + b + c) / 3;
+
+        System.out.println("Average = " + average);
     }
-    
 }
