cad89bd16f5d7fbcf535f4c92384195213769c21
ghgg10345-sys <ghgg10345@gmail.com>
2026-07-01 07:24:26 +0530

    division operation

--- Day1/Problem06.java
+++ Day1/Problem06.java
@@ -2,14 +2,10 @@ package Day1;
 
 public class Problem6 {
     public static void main(String[] args) {
-        int a=-2;
-        int b=2;
-        if(a<b){
-            System.out.println("a is negative");
-        }
-        else{
-            System.out.println("b is negative");
-        }
+        int a = 20;
+        int b = 5;
+        int div = a / b;
+
+        System.out.println("Division = " + div);
     }
-    
 }
