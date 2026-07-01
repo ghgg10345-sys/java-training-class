971af6a8b8731f1119746b7930f1627a4e7ee97a
ghgg10345-sys <ghgg10345@gmail.com>
2026-07-01 07:43:45 +0530

    add two numbers
    Updated the main method to include user input for two numbers and print their sum.

--- Day2/Problem04.java
+++ Day2/Problem04.java
@@ -2,9 +2,15 @@ package Day2;
 
 public class Problem4 {
     public static void main(String[] args) {
-        for(int i=2;i<=10;i=i+2){
-            System.out.println(i);
-        }
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        System.out.println("Sum = " + (a + b));
     }
-    
 }
