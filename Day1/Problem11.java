2a0524b4e4416b74f516a1a45076446f44d6cf99
ghgg10345-sys <ghgg10345@gmail.com>
2026-07-01 07:31:12 +0530

    cube of a number
    Removed unnecessary loop and added cube calculation.

--- Day1/Problem11.java
+++ Day1/Problem11.java
@@ -3,11 +3,9 @@ package Day1;
 public class Problem11 {
     public static void main(String[] args) {
 
-        for(int i=0;i<=2;i++){
-            System.out.println("*");
+        int n = 4;
+        int cube = n * n * n;
 
-        }
-        
+        System.out.println("Cube = " + cube);
     }
-    
 }
