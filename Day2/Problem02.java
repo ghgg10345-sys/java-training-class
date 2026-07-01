1d47b92b4ef2a23f1ef680449c9fc3a9f0492991
ghgg10345-sys <ghgg10345@gmail.com>
2026-07-01 07:40:11 +0530

    print an intiger
    Replaced countdown loop with user input functionality.

--- Day2/Problem02.java
+++ Day2/Problem02.java
@@ -1,10 +1,10 @@
 package Day2;
 
 public class Problem2 {
-    public static void main(String[] args) {
-        for(int i=5;i>=1;i--){
-            System.out.println(i);
-        }
+    public static void main(String[] args) 
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        System.out.println("Number: " + num);
     }
-    
 }
