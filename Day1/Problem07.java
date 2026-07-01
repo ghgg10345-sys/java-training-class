dd12f3760cda6fb7aca35c8e3b35a83953654676
ghgg10345-sys <ghgg10345@gmail.com>
2026-07-01 07:25:37 +0530

    To calculate remainder
    Removed leap year check and added remainder calculation.

--- Day1/Problem07.java
+++ Day1/Problem07.java
@@ -2,14 +2,11 @@ package Day1;
 
 public class Problem7 {
     public static void main(String[] args) {
-        int year=2024;
-        if(year%4==0){
-            System.out.println("leap year");
-        }
-        else{
-            System.out.println("not leap year");
-        }
         
+        int a = 20;
+        int b = 3;
+        int rem = a % b;
+
+        System.out.println("Remainder = " + rem);
     }
-    
 }
